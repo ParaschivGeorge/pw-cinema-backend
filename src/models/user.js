9cc51da34433f9403c6eff810ed366fa7c189646
ParaschivGeorge <georgegmtt@gmail.com>
2019-04-02 23:13:02 +0300

    added user to token

--- src/models/user.js
+++ src/models/user.js
@@ -55,7 +55,7 @@ userSchema.virtual('reviews', {
 // generates json web token for a user with usr id in it
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, 'secret')
+    const token = jwt.sign({ _id: user._id.toString(), sub: user }, 'secret')
     return token
 }
 
